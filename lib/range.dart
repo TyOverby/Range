92a32293c61d1a58ba150c00757d43c1d575d60e
TyOverby <ty@pre-alpha.com>
2013-10-04 21:23:48 -0700

    getting ready to publish to pub

--- lib/range.dart
+++ lib/range.dart
@@ -3,19 +3,19 @@ import 'dart:collection';
 class RangeIterator extends Iterator<num> {
   final num start;
   final num end;
-  final num step;
+  final num interval;
   
   num _current;
   
-  RangeIterator(this.start, this.end, this.step){
-    this._current = this.start - this.step;  
+  RangeIterator(this.start, this.end, this.interval){
+    this._current = this.start - this.interval;
   }
   
   num get current => _current;
   
   bool moveNext() {
-    _current += step;
-    if (step > 0) {
+    _current += interval;
+    if (interval > 0) {
       return _current < end;
     } else {
       return _current > end;
@@ -23,23 +23,86 @@ class RangeIterator extends Iterator<num> {
   }
 }
 
+/**
+  * An iterable that represents a sequence of numbers at an interval.  It
+  * is named similarly to Python's `range` function.
+  *
+  * This class provides an easy way to create a sequence of numbers from a
+  * starting point to an ending point via an interval.
+*/
 class Range extends Iterable<num> with IterableMixin<num> {
-  final num start;
-  final num end;
-  final num step;
-  
-  Range(this.start, this.end, [this.step = 1]);
-  Range.to(this.end, [this.step = 1]):start = 0;
-  Range.toInfinity([this.start = 0, this.step = 1]): end = double.INFINITY;
+  num _start;
+  num _end;
+  num _interval;
 
- 
+/**
+  * The standard way to create a [Range] with all possible options.
+  *
+  *     new Range(start, end, [interval])
+  * returns a [Range] that has the specified start and end points and each value
+  * of the iterator increases by [interval].  The range is inclusive on [start] but
+  * exclusive on [end].
+  *
+  *     new Range(5, 10, 2); // is similar in concept to
+  *     [5, 7, 9]
+*/
+  Range(this._start, this._end, [this._interval = 1]) {
+    if((this._end < this._start && this._interval > 0) ||
+       (this._end > this._start && this._interval < 0) ||
+       (this._interval == 0 || this._interval == double.INFINITY || this._interval == double.NEGATIVE_INFINITY ||
+       this._start == double.INFINITY || this._start == double.NEGATIVE_INFINITY)) {
+      throw new ArgumentError("Impossible range ($_start to $_end by $_interval)"
+      "  Create infinite ranges with Range.infinite.");
+    } else if(this._start == double.NAN) {
+      throw new ArgumentError("Impossible range, start is NaN.");
+    } else if(this._end == double.NAN) {
+      throw new ArgumentError("Impossible range, end is NaN.");
+    } else if (this._interval == double.NAN) {
+      throw new ArgumentError("Impossible range, interval is NaN.");
+    }
+  }
+
+/**
+  * Creates a [Range] starting at 0 to an [end] point.
+  *
+  * Functionally equivalent to
+  *     new Range(0, end, interval);
+  *
+*/
+  factory Range.to(num end, [num interval]) {
+    if(interval == null) {
+      interval = (end > 0) ? 1 : -1;
+    }
+    return new Range(0, end, interval);
+  }
+
+/**
+  * Creates an infinite [Range] that has a [start] point and an [interval] but no end.
+  *
+  * While this constructor can be simulated by a call to the default [Range] constructor,
+  * it is a bit more complicated.  Judging by the sign of the interval the [end]  of the [Range]
+  * will be either be positive or negative.
+  *
+  *     new Range.infinite(5, 2);
+  *     // will produce:
+  *     new Range(5, double.INFINITY, 2);
+  *
+  *     // while
+  *     new Range.infinite(5, -2);
+  *     // will produce:
+  *     new Range(5, double.NEGATIVE_INFINITY, -2);
+  *
+*/
+  factory Range.infinite([num start = 0, num interval = 1]){
+    if(interval > 0) {
+      return new Range(start, double.INFINITY, interval);
+    } else {
+      return new Range(start, double.NEGATIVE_INFINITY, interval);
+    }
+  }
+
   Iterator<num> get iterator {
-    return new RangeIterator(start, end, step);
-  }  
+    return new RangeIterator(_start, _end, _interval);
+  }
 }
 
-void main() {
-  var a = new Range(-5,5);
-  a.forEach(print);
-  print(a.toList());
-}
--- test/range_test.dart
+++ test/range_test.dart
@@ -2,39 +2,100 @@ import 'package:unittest/unittest.dart';
 import '../lib/range.dart';
 
 void main() {
-  group("Finite range | positive increment | integer values", (){
-    test("0 to 10", (){
-      expect(new Range(0,10).toList(), orderedEquals([0,1,2,3,4,5,6,7,8,9]));
+  group("Finite range | positive increment | integer values", () {
+    test("0 to 10", () {
+      expect(new Range(0, 10), orderedEquals([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
     });
-    test("-5 to 5", (){
-      expect(new Range(-5, 5).toList(), orderedEquals([-5,-4,-3,-2,-1,0,1,2,3,4]));
+    test("-5 to 5", () {
+      expect(new Range(-5, 5), orderedEquals([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4]));
     });
-    test("-10 to 0", (){
-      expect(new Range(-10, 0).toList(), orderedEquals([-10,-9,-8,-7,-6,-5,-4,-3,-2,-1]));
+    test("-10 to 0", () {
+      expect(new Range(-10, 0), orderedEquals([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]));
     });
   });
-  
+
   group("Finite range | negative increment | integer values", () {
     test("10 to 0", () {
-      expect(new Range(10, 0, -1).toList(), orderedEquals([10,9,8,7,6,5,4,3,2,1]));
+      expect(new Range(10, 0, -1), orderedEquals([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
     });
-    test("5 to -5", (){
-      expect(new Range(5, -5, -1).toList(), orderedEquals([5,4,3,2,1,0,-1,-2,-3,-4]));
+    test("5 to -5", () {
+      expect(new Range(5, -5, -1), orderedEquals([5, 4, 3, 2, 1, 0, -1, -2, -3, -4]));
     });
-    test("0 to -10", (){
-      expect(new Range(0, -10, -1).toList(), orderedEquals([0,-1,-2,-3,-4,-5,-6,-7,-8,-9]));
-    });
-  });
-  
-  group("Finite range | positive increment | double values", (){
-    test("0 to 5 by 0.5", (){
-      expect(new Range(0,5,0.5).toList(), orderedEquals([0,0.5,1,1.5,2,2.5,3,3.5,4,4.5]));
-    });
-    test("-2 to 2 by 0.5", (){
-      expect(new Range(-2, 2, 0.5).toList(), orderedEquals([-2,-1.5, -1, -0.5,0,0.5,1,1.5]));
-    });
-    test("-5 to 0 by 0.5", (){
-      expect(new Range(-5, 0, 0.5).toList(), orderedEquals([-5,-4.5,-4,-3.5,-3, ]));
+    test("0 to -10", () {
+      expect(new Range(0, -10, -1), orderedEquals([0, -1, -2, -3, -4, -5, -6, -7, -8, -9]));
     });
   });
+
+  group("Finite range | positive increment | double values", () {
+    test("0 to 5 by 0.5", () {
+      expect(new Range(0, 5, 0.5), orderedEquals([0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]));
+    });
+    test("-2 to 2 by 0.5", () {
+      expect(new Range(-2, 2, 0.5), orderedEquals([-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5]));
+    });
+    test("-5 to 0 by 0.5", () {
+      expect(new Range(-5, 0, 0.5), orderedEquals([-5, -4.5, -4, -3.5, -3, -2.5, -2, -1.5, -1, -0.5]));
+    });
+  });
+
+  group("Finite range | negative increment | integer values", () {
+    test("5 to 0 by 0.5", () {
+      expect(new Range(5, 0, -0.5), orderedEquals([5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 0.5]));
+    });
+    test("2 to -2 by 0.5", () {
+      expect(new Range(2, -2, -0.5), orderedEquals([2, 1.5, 1, 0.5, 0, -0.5, -1, -1.5]));
+    });
+    test("0 to -5 by 0.5", () {
+      expect(new Range(0, -5, -0.5), orderedEquals([0, -0.5, -1, -1.5, -2, -2.5, -3, -3.5, -4, -4.5]));
+    });
+  });
+
+  group("Range.toInfinity", () {
+    test("default parameters", () {
+      expect(new Range.infinite(0, 1).take(5), orderedEquals([0, 1, 2, 3, 4]));
+    });
+    test("0 to infinity by two", () {
+      expect(new Range.infinite(0, 2).take(5), orderedEquals([0, 2, 4, 6, 8]));
+    });
+    test("0 to infinity by 0.5", () {
+      expect(new Range.infinite(0, 0.5).take(5), orderedEquals([0, 0.5, 1, 1.5, 2]));
+    });
+    test("0 to negative infinity by 1", () {
+      expect(new Range.infinite(0, -1).take(5), orderedEquals([0, -1, -2, -3, -4]));
+    });
+    test("0 to negative infinity by 0.5", () {
+      expect(new Range.infinite(0, -0.5).take(5), orderedEquals([0, -0.5, -1, -1.5, -2]));
+    });
+  });
+
+  group("Range.to", (){
+    test("5", () {
+      expect(new Range.to(5), orderedEquals([0,1,2,3,4]));
+    });
+    test("-5", (){
+      expect(new Range.to(-5), orderedEquals([0,-1,-2,-3,-4]));
+    });
+
+    test("5 by 2", (){
+      expect(new Range.to(5, 2), orderedEquals([0,2,4]));
+    });
+
+    test("-5 by -2", (){
+      expect(new Range.to(-5, -2), orderedEquals([0,-2,-4]));
+    });
+  });
+
+  group("impossible range", (){
+    test("a to b by negative", (){
+      expect((){new Range(0, 10, -1);}, throwsA(new isInstanceOf<ArgumentError>()));
+    });
+    test("a to b by positive", (){
+      expect((){new Range(0, -10, 1);}, throwsA(new isInstanceOf<ArgumentError>()));
+    });
+    test("a to b by 0", () {
+      expect((){new Range(0, 10, 0);}, throwsA(new isInstanceOf<ArgumentError>()));
+    });
+  });
+
+  //todo(tyoverby): Add tests for the remainder of illegal arguments
 }
